3552bd2077eee454130510f5e9654b902d7c9994
Laura Szymańska <lauraszy@wp.pl>
2024-10-10 21:42:29 +0200

    added login and register form

--- src/views/login/login.component.ts
+++ src/views/login/login.component.ts
@@ -1,17 +1,62 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { 
+  FormControl, 
+  FormGroup, 
+  ReactiveFormsModule 
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { 
+  MatTab, 
+  MatTabGroup 
+} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatCardModule, MatTabGroup, MatTab, MatButtonModule],
+  imports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatCardModule, 
+    MatTabGroup, 
+    MatTab, 
+    MatButtonModule, 
+    MatIconModule,
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  hidePassword = true;
+  hideSignupPassword = true;
+
+  loginForm = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  signupForm = new FormGroup({
+    login: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
+
+  togglePasswordVisibility() {
+
+  }
+
+  onLoginSubmit() {
+    
+  }
+
+  onSignupSubmit() {
+
+  }
 }

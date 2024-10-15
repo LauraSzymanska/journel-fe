91404616bad29fb6effd639e1150ca16c93e8dfc
Laura Szymańska <lauraszy@wp.pl>
2024-10-15 09:22:11 +0200

    definied routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,28 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../views/login/login.component';
 import { HomepageComponent } from '../views/homepage/homepage.component';
+import { ShellComponent } from '../views/base/shell/shell.component';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
+        component: ShellComponent,
+        children: 
+        [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full',
+            },
+            {
+                path: 'home',
+                component: HomepageComponent
+            },
+        ]
     },
     {
         path: 'login',
         component: LoginComponent
     },
-    {
-        path: 'home',
-        component: HomepageComponent
-    }
+
 ];

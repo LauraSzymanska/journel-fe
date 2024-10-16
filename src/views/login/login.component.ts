5d4da51846e951e7b94bb9dc51c87b860cf5cf4b
Laura Szymańska <lauraszy@wp.pl>
2024-10-16 20:49:49 +0200

    minor fixes

--- src/views/login/login.component.ts
+++ src/views/login/login.component.ts
@@ -7,7 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
